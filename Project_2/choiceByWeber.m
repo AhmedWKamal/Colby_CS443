c55ebe45cd1faf5ff39933c1482c470a6bd12d53
emilylarson <emilylarson@users.noreply.github.com>
2020-03-02 11:28:44 -0500

    Train completed

--- Project_2/choiceByWeber.m
+++ Project_2/choiceByWeber.m
@@ -6,4 +6,11 @@ function Tj = choiceByWeber(curr_A, w_code, alpha)
   % curr_A: Current input
   % w_code: Coding layer wts for committed nodes only!
   % alpha: Choice parameter
+  
+  Tj = [];
+  for i = 1:C
+      a = sum(min(w_code(:,i),curr_A),"all");
+      b = (alpha + min(w_code(:,i));
+      Tj = [Tj; a/b];
+    end
 end
